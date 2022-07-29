8de32b5818b92a148638aa9c091b27205b656419
Francisco <franciscodominguezhernandez.76@gmail.com>
2022-07-29 00:42:51 -0500

    Update Sing In

---
+++ app/src/main/java/com/example/yerbabuena/CheckoutView.kt
@@ -0,0 +1,14 @@
+package com.example.yerbabuena
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheckoutView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checkout_view)
+
+    }
+
+
+}
