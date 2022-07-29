6be8b94d6fb44ca3f458fb43ab851e53f99066ef
Alain Eus Rivera <aln.r.r.91@outlook.com>
2022-07-28 19:03:02 -0500

    Custom login validations

--- app/src/main/java/com/example/yerbabuena/SignInActivity.kt
+++ app/src/main/java/com/example/yerbabuena/SignInActivity.kt
@@ -4,17 +4,24 @@ import android.app.AlertDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import java.util.regex.Pattern
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
+
+        val email = findViewById<EditText>(R.id.FieldEmail)
+        val password = findViewById<EditText>(R.id.FieldPassword)
+        email.setText(Firebase.auth.currentUser?.email)
+        val fields = arrayListOf<EditText>(email, password)
 
         val signup: Button = findViewById(R.id.btn_signup)
         signup.setOnClickListener {
@@ -22,67 +29,66 @@ class SignInActivity : AppCompatActivity() {
             finish()
         }
 
-        val email = (findViewById<EditText>(R.id.FieldEmail))
-        email.setText(Firebase.auth.currentUser?.email)
-
-        var pwdreset = findViewById<TextView>(R.id.linkForgotPassword)
+        val pwdreset = findViewById<TextView>(R.id.linkForgotPassword)
         pwdreset.setOnClickListener {
             if (email.text.isNotEmpty()) {
                 sendPasswordResetEmail()
             }
         }
 
-        if (!Firebase.auth.currentUser!!.isEmailVerified) {
-            sendVerificationEmail()
-        }
-
         val signIn = findViewById<Button>(R.id.btnLogin)
         signIn.setOnClickListener {
-            val email: String = (findViewById<EditText>(R.id.FieldEmail)).text.toString()
-            val password: String = (findViewById<EditText>(R.id.FieldPassword)).text.toString()
-
-            Firebase.auth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        if (!Firebase.auth.currentUser!!.isEmailVerified) {
-                            sendVerificationEmail()
-                        } else {
-                            startActivity(Intent(applicationContext, MainActivity::class.java))
-                            finish()
-                        }
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Toast.makeText(
-                            applicationContext,
-                            "${task.exception?.message}",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
+            var isFieldEmpty = false
+            fields.forEach { item ->
+                item.setText(item.text.trim())
+                if ( item.text.isEmpty() ) {
+                    item.error = getString(R.string.empty_field)
+                    isFieldEmpty = true
                 }
+            }
+
+            if (isFieldEmpty) return@setOnClickListener
+
+            else if (!Patterns.EMAIL_ADDRESS.matcher(email.text).matches()) {
+                email.error = getString(R.string.invalid_email)
+            }
+            else signIn(email.text.toString(), password.text.toString())
         }
     }
 
+    private fun signIn(email: String, password: String)
+    {
+        Firebase.auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success
+                    if (!Firebase.auth.currentUser!!.isEmailVerified) {
+                        sendVerificationEmail()
+                    } else {
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    }
+                } else {
+                    // Sign in fails, display a message to the user.
+                    Toast.makeText(this, "${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
     private fun sendVerificationEmail() {
-        var builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
         builder.setTitle(getString(R.string.email_not_verified_title))
-            .setMessage(
-                getString(
+            .setMessage(getString(
                     R.string.email_not_verified_message,
                     Firebase.auth.currentUser?.email
-                )
-            )
+                ))
         builder.setCancelable(false)
 
         builder.setPositiveButton("Si") { dialog, _ ->
             Firebase.auth.currentUser!!.sendEmailVerification().addOnSuccessListener {
-                Toast.makeText(
-                    applicationContext,
-                    getText(R.string.email_verification_sent),
-                    Toast.LENGTH_SHORT
-                ).show()
+                Toast.makeText(this, getText(R.string.email_verification_sent), Toast.LENGTH_SHORT).show()
             }.addOnFailureListener { ex: Exception ->
-                Toast.makeText(applicationContext, "${ex.message}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "${ex.message}", Toast.LENGTH_SHORT).show()
             }
             dialog.dismiss()
         }
@@ -94,7 +100,7 @@ class SignInActivity : AppCompatActivity() {
 
     private fun sendPasswordResetEmail()
     {
-        var builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
         builder.setTitle(getString(R.string.email_password_reset_title))
             .setMessage(R.string.email_password_reset_message)
 
@@ -102,8 +108,7 @@ class SignInActivity : AppCompatActivity() {
         builder.setPositiveButton("Si") { dialog, _ ->
             val email = (findViewById<EditText>(R.id.FieldEmail))
             Firebase.auth.sendPasswordResetEmail(email.text.toString()).addOnSuccessListener {
-                Toast.makeText(this, R.string.email_password_reset_sent, Toast.LENGTH_SHORT)
-                    .show()
+                Toast.makeText(this, R.string.email_password_reset_sent, Toast.LENGTH_SHORT).show()
             }.addOnFailureListener {
                 Toast.makeText(this, "${it.message}", Toast.LENGTH_SHORT).show()
             }
@@ -113,5 +118,12 @@ class SignInActivity : AppCompatActivity() {
             dialog.dismiss()
         }
         builder.create().show()
+    }
+
+    private fun CharSequence.isValidPassword(): Boolean {
+        val passwordPattern = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$"
+        val pattern = Pattern.compile(passwordPattern)
+        val matcher = pattern.matcher(this)
+        return matcher.matches()
     }
 }
--- app/src/main/java/com/example/yerbabuena/SignUpActivity.kt
+++ app/src/main/java/com/example/yerbabuena/SignUpActivity.kt
@@ -3,83 +3,139 @@ package com.example.yerbabuena
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.actionCodeSettings
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.IgnoreExtraProperties
 import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
+import java.util.regex.Pattern
 
 class SignUpActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var btnRegister: Button
 
     @IgnoreExtraProperties
-    data class User(val name: String? = null,
-                    val surname: String? = null,
-                    val phone: String? = null,
-                    val email: String? = null,
-                    val role: String? = "Cliente") {
+    data class Ubicacion(
+        val latitud: Double? = null,
+        val longitud: Double? = null
+    )
+
+    @IgnoreExtraProperties
+    data class Usuario(
+        val name: String? = null,
+        val surname: String? = null,
+        val phone: String? = null,
+        val email: String? = null,
+        val role: String? = null,
+        val location: Ubicacion? = null
+    ) {
         // Null default values create a no-argument default constructor, which is needed
         // for deserialization from a DataSnapshot.
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
+        val name = (findViewById<EditText>(R.id.FieldName))
+        val surname = (findViewById<EditText>(R.id.FieldSurnames))
+        val phone = (findViewById<EditText>(R.id.FieldPhoneNumber))
+        val email = (findViewById<EditText>(R.id.FieldEmail))
+        val password = (findViewById<EditText>(R.id.FieldPassword))
+        val pwdvalid = findViewById<EditText>(R.id.FieldConfirmPassword)
+
         // Initialize Firebase Auth
         auth = Firebase.auth
 
-        val loginbtn: Button = findViewById(R.id.btnLogin)
-        loginbtn.setOnClickListener {
+        val fields = arrayListOf<EditText>(name, surname, phone, email, password, pwdvalid)
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener {
             startActivity(Intent(applicationContext, SignInActivity::class.java))
             finish()
         }
 
-        val register: Button = findViewById(R.id.btnRegister)
-        register.setOnClickListener {
-            val name: String = (findViewById<EditText>(R.id.FieldName)).text.toString()
-            val surname: String = (findViewById<EditText>(R.id.FieldSurnames)).text.toString()
-            val phone: String = (findViewById<EditText>(R.id.FieldPhoneNumber)).text.toString()
-            val email: String = (findViewById<EditText>(R.id.FieldEmail)).text.toString()
-            val password: String = (findViewById<EditText>(R.id.FieldPassword)).text.toString()
-
-            auth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        saveUserData(name, surname, phone, email)
-                        task.result.user?.sendEmailVerification()?.addOnSuccessListener{
-                            //auth.signOut()
-                            startActivity(Intent(applicationContext, SignInActivity::class.java))
-                            finish()
-                        }?.addOnFailureListener{
-                            Toast.makeText(applicationContext, "${it.message}", Toast.LENGTH_SHORT).show()
-                        }
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Toast.makeText(applicationContext, "${task.exception?.message}", Toast.LENGTH_SHORT).show()
-                    }
+        btnRegister = findViewById(R.id.btnRegister)
+        btnRegister.setOnClickListener {
+            var isFieldEmpty = false
+            fields.forEach { item ->
+                item.setText(item.text.trim())
+                if ( item.text.isEmpty() ) {
+                    item.error = getString(R.string.empty_field)
+                    isFieldEmpty = true
                 }
+            }
+            if (isFieldEmpty) return@setOnClickListener
+            if (!Patterns.PHONE.matcher(phone.text).matches()) {
+                phone.error = getString(R.string.invalid_phone)
+            } else if (!Patterns.EMAIL_ADDRESS.matcher(email.text).matches()) {
+                email.error = getString(R.string.invalid_email)
+            } else if (!password.text.isValidPassword()) {
+                password.error = getString(R.string.invalid_password)
+            } else if (!password.text.contains(pwdvalid.text, false) ||
+                !pwdvalid.text.contains(password.text, false)) {
+                Toast.makeText(this, R.string.passwords_not_match, Toast.LENGTH_SHORT).show()
+            } else {
+                btnRegister.isEnabled = false
+                signUp(name.text.toString(),
+                    surname.text.toString(),
+                    phone.text.toString(),
+                    email.text.toString(),
+                    password.text.toString())
+            }
         }
     }
 
-    private fun saveUserData(name:String, surname:String, phone:String, email:String)
-    {
-        var usuario = User(name, surname, phone, email)
-        val database = Firebase.database
-        val myRef = database.getReference("/")
+    private fun signUp(name: String,
+                       surname: String,
+                       phone: String,
+                       email: String,
+                       password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    task.result.user?.sendEmailVerification()?.addOnSuccessListener {
+                        Toast.makeText(this, R.string.email_verification_sent, Toast.LENGTH_LONG).show()
+                    }?.addOnFailureListener {
+                        Toast.makeText(this, "${it.message}", Toast.LENGTH_SHORT).show()
+                    }
+                    saveUserData(name, surname, phone, email)
+                    startActivity(Intent(this, SignInActivity::class.java))
+                    finish()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this, "${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                    btnRegister.isEnabled = true
+                }
+            }
+    }
+
+    private fun saveUserData(
+        name: String,
+        surname: String,
+        phone: String,
+        email: String
+    ) {
+        val ubicacion = Ubicacion(0.0, 0.0)
+        val usuario = Usuario(name, surname, phone, email, getString(R.string.default_role), ubicacion)
+        val myRef = Firebase.database.getReference("/")
         myRef.child("Usuarios").push().setValue(usuario)
-            .addOnSuccessListener {
-                // Write was successful!
-                Toast.makeText(applicationContext, "ok", Toast.LENGTH_SHORT).show()
-            }
             .addOnFailureListener {
-                // Write failed
-                Toast.makeText(applicationContext, "${it.message}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "${it.message}", Toast.LENGTH_SHORT).show()
             }
+    }
+
+    private fun CharSequence.isValidPassword(): Boolean {
+        val passwordPattern = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$"
+        val pattern = Pattern.compile(passwordPattern)
+        val matcher = pattern.matcher(this)
+        return matcher.matches()
     }
 }
